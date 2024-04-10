5121cede5c948cd27e593a1200d4bb5db1559088
LejoA95 <alejoachicanoy95@gmail.com>
2024-04-10 12:30:29 -0500

    portafolio

---
+++ src/app/ui/fonts.jsx
@@ -0,0 +1,8 @@
+import { Inter, Open_Sans, Noto_Sans } from 'next/font/google'
+
+
+export const inter = Inter({subsets: ['latin']})
+
+export const openSans = Open_Sans({subsets: ['latin']})
+
+export const NotoSans = Noto_Sans({subsets: ['latin']})
